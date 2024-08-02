7cd3722b9c225b317b6cf5af727fd784d5a0e415
OMARTK1 <elyazidi.omar@gmail.com>
2024-08-02 02:16:36 +0100

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    box_checked = set()
+    F1_box = [0]
+
+    while F1_box:
+        curnt_box = F1_box.pop()
+        if curnt_box not in box_checked:
+            box_checked.add(curnt_box)
+            for key in boxes[curnt_box]:
+                if key < len(boxes):
+                    F1_box.append(key)
+
+    return len(box_checked) == len(boxes)
