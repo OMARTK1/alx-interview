de14b90cfd0e8f83fdae5d9e3ac92962fe28e166
OMARTK1 <elyazidi.omar@gmail.com>
2024-08-09 00:35:51 +0100

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,24 +4,25 @@ Module that find the minimum number of operations
 and result in exactly n H characters into file
 """
 
+
 def minOperations(n):
     """
     Function that calculates the fewest number of operations needed
     and result in exactly n H characters in the file
-    
+
     :args n: The target number of H characters to be displayed
     :return: The minimum number of operations or 0 if impossible
     """
     if n <= 1:
         return 0
-    
+
     opers = 0
     divs = 2
-    
+
     while n > 1:
         while n % divs == 0:
             opers += divs
             n //= divs
         divs += 1
-    
+
     return opers
